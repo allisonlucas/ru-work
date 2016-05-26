546de23fbc40d08aacf005a27d6ad3546265a0ba
Allison Dean <allisonlaureldean@gmail.com>
2016-05-26 23:38:29 +0000

    js5 commit3

--- Exercises/js5-edit-in-place/js5.js
+++ Exercises/js5-edit-in-place/js5.js
@@ -1,22 +1,24 @@
-var profileHeaderDiv = document.querySelectorAll('.small-header')[0];
+var usernameHeaderDiv = document.querySelectorAll('.small-header')[0];
 var username = document.querySelector('.small-header p');
 
 username.addEventListener("click", function(){
     username.style = "display: none";
     var newUserDiv = document.createElement('div');
     newUserDiv.innerHTML = '<input type="text" onblur="if(value=="") value = "Username" onfocus="if(value=="Username") value = """/>';
-    profileHeaderDiv.appendChild(newUserDiv);
-    // if(h3Div.length > 0) {   
-    //     h3Div.document.appendChild('username')
-    //     alert(h3Div)
-    // }
+    usernameHeaderDiv.appendChild(newUserDiv);
 });
 
-// var username = document.getElementById('username');
+var inlineDiv = document.querySelectorAll('.inline')[0];
+var firstName = document.querySelector('.inline p');
 
-// if (username.length > 0) {
-//     var newh2 = document.querySelector('h2')[0];
-    
-// }
+firstName.addEventListener("click", function(){
+    // firstName.style = "visibility: hidden";
+    firstName.style = "display: none";
+    var newFirstDiv = document.createElement('div');
+    newFirstDiv.innerHTML = '<input type="text" onblur="if(value=="") value = "First name" onfocus="if(value=="First name") value = """/>';
+    inlineDiv.appendChild(newFirstDiv);
+});
+
+
 
 
