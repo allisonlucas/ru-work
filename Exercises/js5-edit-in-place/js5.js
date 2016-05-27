bba55cb09298e9d59cdb9da1e46a63779c91ad45
Allison Dean <allisonlaureldean@gmail.com>
2016-05-27 03:41:17 +0000

    js5 commit4

--- Exercises/js5-edit-in-place/js5.js
+++ Exercises/js5-edit-in-place/js5.js
@@ -12,7 +12,6 @@ var firstNameDiv = document.querySelectorAll('.first-name')[0];
 var firstName = document.querySelector('.first-name p');
 
 firstName.addEventListener("click", function(){
-    // firstName.style = "visibility: hidden";
     firstName.style = "display: none";
     var newFirstDiv = document.createElement('div');
     newFirstDiv.innerHTML = '<input type="text" onblur="if(value=="") value = "First name" onfocus="if(value=="First name") value = """/>';
@@ -23,13 +22,40 @@ var lastNameDiv = document.querySelectorAll('.last-name')[0];
 var lastName = document.querySelector('.last-name p');
 
 lastName.addEventListener("click", function(){
-    // firstName.style = "visibility: hidden";
     lastName.style = "display: none";
     var newLastDiv = document.createElement('div');
     newLastDiv.innerHTML = '<input type="text" onblur="if(value=="") value = "Last name" onfocus="if(value=="Last name") value = """/>';
     lastNameDiv.appendChild(newLastDiv);
 });
 
+var cityDiv = document.querySelectorAll('.city')[0];
+var city = document.querySelector('.city p');
 
+city.addEventListener("click", function(){
+    city.style = "display: none";
+    var newCityDiv = document.createElement('div');
+    newCityDiv.innerHTML = '<input type="text" onblur="if(value=="") value = "Current city" onfocus="if(value=="Current city") value = """/>';
+    cityDiv.appendChild(newCityDiv);
+});
+
+var jobTitleDiv = document.querySelectorAll('.job-title')[0];
+var jobTitle = document.querySelector('.job-title p');
+
+jobTitle.addEventListener("click", function(){
+    jobTitle.style = "display: none";
+    var newJobDiv = document.createElement('div');
+    newJobDiv.innerHTML = '<input type="text" onblur="if(value=="") value = "Job title" onfocus="if(value=="Job title") value = """/>';
+    jobTitleDiv.appendChild(newJobDiv);
+});
+
+var companyDiv = document.querySelectorAll('.company')[0];
+var company = document.querySelector('.company p');
+
+company.addEventListener("click", function(){
+    company.style = "display: none";
+    var newCompanyDiv = document.createElement('div');
+    newCompanyDiv.innerHTML = '<input type="text" onblur="if(value=="") value = "Company" onfocus="if(value=="Company") value = """/>';
+    companyDiv.appendChild(newCompanyDiv);
+});
 
 
