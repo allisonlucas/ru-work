d598ae5e29155df34dd26726f14d58af2a604226
Allison Dean <allisonlaureldean@gmail.com>
2016-05-28 04:06:51 +0000

    loop practice complete

---
+++ Exercises/loop-practice/loop-practice.js
@@ -0,0 +1,46 @@
+
+// Loop 1 should print: 0 100 200 300 400 500
+
+// for(var i = 0; i <= 500; i += 100){
+//     console.log(i);
+// }
+
+// Loop 2 should print: 1 2 4 8 16 32 64
+
+// for(var i = 1; i <= 64; i *= 2) {
+//     console.log(i);
+// }
+
+// Loop 3 should print: 1 1 1 2 2 2 3 3 3
+
+// for(var i = 1; i <= 3; i++) {
+//     for(var j = 1; j <= 3; j++){
+//         console.log(i);
+//     }
+// }
+
+// Loop 4 should print: 0 2 4 6 8 10
+
+// for(var i = 0; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+// Loop 5 should print: 3 6 9 12 15
+
+// for(var i = 3; i <= 15; i += 3) {
+//     console.log(i);
+// }
+
+// Loop 6 should print: 9 8 7 6 5 4 3 2 1 0
+
+// for(var i = 9; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// Loop 7 should print: 0 1 2 3 0 1 2 3 0 1 2 3
+
+// for(var i = 0; i <= 3; i++) {
+//     for(var j = 0; j <= 3; j++){
+//         console.log(j);
+//     }
+// }
