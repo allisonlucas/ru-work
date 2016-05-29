334802c5c282ec31c2c6c7b249f45732ccdca76e
Allison Dean <allisonlaureldean@gmail.com>
2016-05-29 04:42:13 +0000

    js5 commit7

--- Exercises/js5-edit-in-place/js5.js
+++ Exercises/js5-edit-in-place/js5.js
@@ -2,116 +2,82 @@ var usernameHeaderDiv = document.querySelectorAll('.small-header')[0];
 var username = document.querySelector('.small-header p');
 
 username.addEventListener("click", function(){
-    username.style = "display: none";
-    var newUserDiv = document.createElement('div');
-    newUserDiv.id = "newUserDiv";
-    newUserDiv.innerHTML = '<input id="newUser" type="text" placeholder = "Username" style="border:none"/>';
-    usernameHeaderDiv.appendChild(newUserDiv);
+    usernameHeaderDiv.innerHTML = '<input id="newUser" type="text" placeholder = "Username" style="border:none"/>';
     document.getElementById("newUser").focus();
-    var newUserValue = document.getElementById("newUser").value;
-    var getUserDiv = document.getElementById("newUserDiv");
-    if(newUserValue.length > 0) {
-        username.addEventListener("onblur", function() {
-        username.replaceChild(newUserValue, username);
-        username.style = "display: block";
-        getUserDiv.style = "display: none";
-        });
-    }
 });
+
+usernameHeaderDiv.addEventListener("blur", function(){
+    var newUsername = document.getElementById("newUser").value
+    usernameHeaderDiv.innerHTML = newUsername
+    alert(usernameHeaderDiv.innerHTML)
+}, true)
 
 var firstNameDiv = document.querySelectorAll('.first-name')[0];
 var firstName = document.querySelector('.first-name p');
 
 firstName.addEventListener("click", function(){
-    firstName.style = "display: none";
-    var newFirstDiv = document.createElement('div');
-    newFirstDiv.id = "newFirstDiv";
-    newFirstDiv.innerHTML = '<input id="newFirst" type="text" placeholder = "First name" style="border:none"/>';
-    firstNameDiv.appendChild(newFirstDiv);
+    firstNameDiv.innerHTML = '<input id="newFirst" type="text" placeholder = "First name" style="border:none"/>';
     document.getElementById("newFirst").focus();
 });
+
+firstNameDiv.addEventListener("blur", function(){
+    var newFirstName = document.getElementById("newFirst").value
+    firstNameDiv.innerHTML = newFirstName
+    alert(firstNameDiv.innerHTML)
+}, true)
 
 var lastNameDiv = document.querySelectorAll('.last-name')[0];
 var lastName = document.querySelector('.last-name p');
 
 lastName.addEventListener("click", function(){
-    lastName.style = "display: none";
-    var newLastDiv = document.createElement('div');
-    newLastDiv.id = "newLastDiv";
-    newLastDiv.innerHTML = '<input id="newLast" type="text" placeholder = "Last name" style="border:none"/>';
-    lastNameDiv.appendChild(newLastDiv);
+    lastNameDiv.innerHTML = '<input id="newLast" type="text" placeholder = "Last name" style="border:none"/>';
     document.getElementById("newLast").focus();
-    var newLastValue = document.getElementById("newLast").value;
-    var getLastDiv = document.getElementById("newLastDiv");
-    
-    if(newLastValue.length > 0) {
-        lastName.addEventListener("onblur", function() {
-            lastName.replaceChild(newLastValue, lastName);
-            lastName.style = "display: block";
-            getLastDiv.style = "display: none";
-        });
-    }
 });
+
+lastNameDiv.addEventListener("blur", function(){
+    var newLastName = document.getElementById("newLast").value
+    lastNameDiv.innerHTML = newLastName
+    alert(lastNameDiv.innerHTML)
+}, true)
 
 var cityDiv = document.querySelectorAll('.city')[0];
 var city = document.querySelector('.city p');
 
 city.addEventListener("click", function(){
-    city.style = "display: none";
-    var newCityDiv = document.createElement('div');
-    newCityDiv.id = "newCityDiv";
-    newCityDiv.innerHTML = '<input id="newCity" type="text" placeholder = "Current city" style="border:none"/>';
-    cityDiv.appendChild(newCityDiv);
+    cityDiv.innerHTML = '<input id="newCity" type="text" placeholder = "Current city" style="border:none"/>';
     document.getElementById("newCity").focus();
-    var cityValue = document.getElementById("newCity").value;
-    var getCityDiv = document.getElementById("newCityDiv");
-        if(cityValue.length > 0) {
-        city.addEventListener("onblur", function() {
-            city.replaceChild(cityValue, city);
-            city.style = "display: block";
-            getCityDiv.style = "display: none";
-            });
-        }
 });
+
+cityDiv.addEventListener("blur", function(){
+    var newCity = document.getElementById("newCity").value
+    cityDiv.innerHTML = newCity
+    alert(cityDiv.innerHTML)
+}, true)
 
 var jobTitleDiv = document.querySelectorAll('.job-title')[0];
 var jobTitle = document.querySelector('.job-title p');
 
 jobTitle.addEventListener("click", function(){
-    jobTitle.style = "display: none";
-    var newJobDiv = document.createElement('div');
-    newJobDiv.id = "newJobDiv";
-    newJobDiv.innerHTML = '<input id="newJob" type="text" placeholder = "Job title" style="border:none"/>';
-    jobTitleDiv.appendChild(newJobDiv);
+    jobTitleDiv.innerHTML = '<input id="newJob" type="text" placeholder = "Job title" style="border:none"/>';
     document.getElementById("newJob").focus();
-    var jobTitleValue = document.getElementById("newJob").value;
-    var getJobTitleDiv = document.getElementById("newJobDiv");
-        if(jobTitleValue.length > 0) {
-        jobTitle.addEventListener("onblur", function() {
-            jobTitle.replaceChild(jobTitleValue, jobTitle);
-            jobTitle.style = "display: block";
-            getJobTitleDiv.style = "display: none";
-            });
-        }
 });
+
+jobTitleDiv.addEventListener("blur", function(){
+    var newJobTitle = document.getElementById("newJob").value
+    jobTitleDiv.innerHTML = newJobTitle
+    alert(jobTitleDiv.innerHTML)
+}, true)
 
 var companyDiv = document.querySelectorAll('.company')[0];
 var company = document.querySelector('.company p');
 
 company.addEventListener("click", function(){
-    company.style = "display: none";
-    var newCompanyDiv = document.createElement('div');
-    newCompanyDiv.id = "newCompanyDiv";
-    newCompanyDiv.innerHTML = '<input id="newCompany" type="text" placeholder = "Company" style="border:none"/>';
-    companyDiv.appendChild(newCompanyDiv);
+    companyDiv.innerHTML = '<input id="newCompany" type="text" placeholder = "Company" style="border:none"/>';
     document.getElementById("newCompany").focus();
-    var companyValue = document.getElementById("newCompany").value;
-    var getCompanyDiv = document.getElementById("newCompanyDiv");
-    if(companyValue.length > 0) {
-        company.addEventListener("onblur", function() {
-        company.replaceChild(companyValue, company);
-        company.style = "display: block";
-        getCompanyDiv.style = "display: none";
-        });
-    }
 });
+
+companyDiv.addEventListener("blur", function(){
+    var newCompany = document.getElementById("newCompany").value
+    companyDiv.innerHTML = newCompany
+    alert(companyDiv.innerHTML)
+}, true)
