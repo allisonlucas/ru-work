aa95e53d8f6a9c3e0f5743e104b3bc2ece57232c
Allison Dean <allisonlaureldean@gmail.com>
2016-06-01 22:00:47 +0000

    js4.1 commit2 confirm

--- Exercises/js4.1-intro-to-angular/js4.1.js
+++ Exercises/js4.1-intro-to-angular/js4.1.js
@@ -36,26 +36,56 @@ function turnPink() {
     
     trnPnk.myfunc1 = function(event){
         event.target.style.color = 'pink'
+        // Show bang
+        trnPnk.bang1 = false
+        trnPnk.bang1 = function(){
+            trnPnk.bang = true
+        }
     }
     
     trnPnk.myfunc2 = function(event){
         event.target.style.color = 'pink'
+        // Show bang
+        trnPnk.bang2 = false
+        trnPnk.bang2 = function(){
+            trnPnk.bang = true
+        }
     }
     
     trnPnk.myfunc3 = function(event){
         event.target.style.color = 'pink'
+        // Show bang
+        trnPnk.bang3 = false
+        trnPnk.bang3 = function(){
+            trnPnk.bang = true
+        }
     }
     
     trnPnk.onLeave1 = function(event){
         event.target.style.color = 'black'
+        // Hide bang
+        trnPnk.bang1 = false
     }
     
     trnPnk.onLeave2 = function(event){
         event.target.style.color = 'black'
+        // Hide bang
+        trnPnk.bang2 = false
     }
     
     trnPnk.onLeave3 = function(event){
         event.target.style.color = 'black'
+        // Hide bang
+        trnPnk.bang3 = false
+    }
+    
+    // Click confirm
+    trnPnk.clickConfirm = function(event){
+        var val = confirm('Would you like to visit the Google site?')
+        if (val == false) {
+            event.preventDefault()
+        }
     }
 }
+
 
