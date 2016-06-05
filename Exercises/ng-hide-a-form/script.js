b52db0dc9c87976e251bac01843647ecb1f35767
Allison Dean <allisonlaureldean@gmail.com>
2016-06-05 19:03:24 +0000

    ng-hide-a-form commit3

--- Exercises/ng-hide-a-form/script.js
+++ Exercises/ng-hide-a-form/script.js
@@ -5,13 +5,16 @@ function formCtrl() {
     var fCtrl = this;
     
     fCtrl.formArr = []
-    // console.log(formArr)
-    fCtrl.text = ''
+    fCtrl.name = ''
+    fCtrl.bio = ''
+    fCtrl.books = ''
+    fCtrl.libraries = ''
+    
     fCtrl.submit = function() {
-        if (fCtrl.text) {
-          fCtrl.formArr.push(fCtrl.text);
-          console.log(fCtrl.formArr)
-          fCtrl.text = '';
-        }
-      };
+            fCtrl.formArr.push(fCtrl.name, fCtrl.bio, fCtrl.books, fCtrl.libraries)
+            fCtrl.name = ''
+            fCtrl.bio = ''
+            fCtrl.books = ''
+            fCtrl.libraries = ''
+    };
 }
