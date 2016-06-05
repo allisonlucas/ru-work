c7dd2291f809a3f09a173c58db69290b333c1e3d
Allison Dean <allisonlaureldean@gmail.com>
2016-06-05 22:36:53 +0000

    ng-hide-a-form complete

--- Exercises/ng-hide-a-form/script.js
+++ Exercises/ng-hide-a-form/script.js
@@ -21,11 +21,38 @@ function formCtrl() {
     fCtrl.books = ''
     fCtrl.libraries = ''
     
-    fCtrl.submit = function() {
-        fCtrl.formArr.push(fCtrl.name, fCtrl.bio, fCtrl.books, fCtrl.libraries)
-        fCtrl.name = ''
-        fCtrl.bio = ''
-        fCtrl.books = ''
-        fCtrl.libraries = ''
+    fCtrl.changeName = function() {
+        if (fCtrl.formArr.name !== null) {
+            fCtrl.formArr.splice(0, 1, fCtrl.name)
+        } else {
+            fCtrl.formArr.push(fCtrl.name)
+        }
     };
+    
+    console.log(fCtrl.formArr)
+    
+    fCtrl.changeBio = function() {
+        if (fCtrl.formArr.bio !== null) {
+            fCtrl.formArr.splice(1, 1, fCtrl.bio)
+        } else {
+            fCtrl.formArr.push(fCtrl.bio)
+        }
+    };
+    
+    fCtrl.changeBooks = function() {
+        if (fCtrl.formArr.books !== null) {
+            fCtrl.formArr.splice(2, 1, fCtrl.books)
+        } else {
+            fCtrl.formArr.push(fCtrl.books)
+        }
+    };
+
+    fCtrl.changeLibraries = function() {
+        if (fCtrl.formArr.libraries !== null) {
+            fCtrl.formArr.splice(3, 1, fCtrl.libraries)
+        } else {
+            fCtrl.formArr.push(fCtrl.libraries)
+        }
+    };
+
 }
