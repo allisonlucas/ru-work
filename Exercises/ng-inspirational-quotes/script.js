983b057cfe5899ff0837a9890c23ade1d4b15b18
Allison Dean <allisonlaureldean@gmail.com>
2016-06-07 22:27:40 +0000

    ng-inspriational-quotes commit2

--- Exercises/ng-inspirational-quotes/script.js
+++ Exercises/ng-inspirational-quotes/script.js
@@ -15,7 +15,32 @@ function quotesCtrl() {
         }
     }
     
-    qCtrl.formArr = []
+    qCtrl.formArr = [
+        {
+            quote: "Quote 1",
+            author: "Author 1"
+        },
+        
+        {
+            quote: "Quote 2",
+            author: "Author 2"    
+        },
+        
+        {
+            quote: "Quote 3",
+            author: "Author 3"
+        },
+        
+        {
+            quote: "Quote 4",
+            author: "Author 4"
+        },
+        
+        {
+            quote: "Quote 5",
+            author: "Author 5"
+        }
+        ]
     qCtrl.quote = ''
     qCtrl.author = ''
     
@@ -30,5 +55,9 @@ function quotesCtrl() {
             qCtrl.quote = ''
             qCtrl.author = ''
         }
+    }
+    
+    qCtrl.delete = function(index) {
+        qCtrl.formArr.splice(index, 1)
     }
 }
