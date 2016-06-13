35367aa1a9dbcc491f256c26a2d2f734d7261650
Allison Dean <allisonlaureldean@gmail.com>
2016-06-13 22:54:06 +0000

    midterm project commit2

--- Projects/midterm_chart/chart.js
+++ Projects/midterm_chart/chart.js
@@ -108,98 +108,139 @@
 var lineData = [
   {
   x: 1,
-  y: 142
+  y: 142,
+  z: 75
 }, {
   x: 2,
-  y: 146
+  y: 146,
+  z: 76
 }, {
   x: 3,
-  y: 147
+  y: 147,
+  z: 76
 }, {
   x: 4,
-  y: 145
+  y: 145,
+  z: 75
 }, {
   x: 5,
-  y: 143
+  y: 143,
+  z: 73
 }, {
   x: 6,
-  y: 140
+  y: 140,
+  z: 73
 }, {
   x: 7,
-  y: 141
+  y: 141,
+  z: 71
 }, {
   x: 8,
-  y: 143
+  y: 143,
+  z: 72
 }, {
   x: 9,
-  y: 144
+  y: 144,
+  z: 72
 },
   {
   x: 10,
-  y: 142
+  y: 142,
+  z: 78
 }, {
   x: 11,
-  y: 142
+  y: 142,
+  z: 78
 },
 {
   x: 12,
-  y: 146
+  y: 146,
+  z: 77
 }, {
   x: 13,
-  y: 147
+  y: 147,
+  z: 77
 }, {
   x: 14,
-  y: 145
+  y: 145,
+  z: 76
 }, {
   x: 15,
-  y: 143
+  y: 143,
+  z: 77
 }, {
   x: 16,
-  y: 140
+  y: 140,
+  z: 75
 }, {
   x: 17,
-  y: 141
+  y: 141,
+  z: 75
 }, {
   x: 18,
-  y: 143
+  y: 143,
+  z: 74
 }, {
   x: 19,
-  y: 144
+  y: 144,
+  z: 74
 }, {
   x: 20,
-  y: 142
+  y: 142,
+  z: 73
 }, {
   x: 21,
-  y: 142
+  y: 142,
+  z: 73
 }, {
   x: 22,
-  y: 146
+  y: 146,
+  z: 71
 }, {
   x: 23,
-  y: 147
+  y: 147,
+  z: 68
 }, {
   x: 24,
-  y: 145
+  y: 145,
+  z: 75
 }, {
   x: 25,
-  y: 143
+  y: 143,
+  z: 75
 }, {
   x: 26,
-  y: 140
+  y: 140,
+  z: 76
 }, {
   x: 27,
-  y: 141
+  y: 141,
+  z: 76
 }, {
   x: 28,
-  y: 143
+  y: 143,
+  z: 77
 }, {
   x: 29,
-  y: 144
+  y: 144,
+  z: 75
 }, {
   x: 30,
-  y: 144
+  y: 144,
+  z: 76
 }
 ];
+
+var idealTemp = [
+  {
+    a: 140,
+    x: 0
+  },
+  {
+    a: 160,
+    x: 31
+  }
+  ];
 
 var vis = d3.select('#visualisation'),
     WIDTH = 500,
@@ -210,12 +251,27 @@ var vis = d3.select('#visualisation'),
       bottom: 20,
       left: 50
     },
-    xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([1, 31]),
-    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function(d) {
-      return d.y -20;
+    xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(lineData, function(d) {
+      return (d.x);
     }), d3.max(lineData, function(d) {
-      return d.y + 20;
+      return (d.x);
     })]),
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function(d) {
+      return Math.min(d.y - 20, d.z - 20);
+    }), d3.max(lineData, function(d) {
+      return Math.max(d.y + 20, d.z + 20);
+    })]),
+    // xRangeIdealTemp = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(idealTemp, function(d) {
+    //   return (d.x);
+    // }), d3.max(idealTemp, function(d) {
+    //   return (d.x);
+    // })]),
+    // yRangeIdealTempMin = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(idealTemp, function(d) {
+    //   return (d.a);
+    // })]),
+    // yRangeIdealTempMax = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.max(idealTemp, function(d) {
+    //   return (d.a);
+    // })]),
     xAxis = d3.svg.axis()
       .scale(xRange)
       .tickSize(3)
@@ -236,7 +292,7 @@ vis.append('svg:g')
   .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
   .call(yAxis);
   
-  var lineFunc = d3.svg.line()
+var lineFunc = d3.svg.line()
   .x(function(d) {
     return xRange(d.x);
   })
@@ -249,7 +305,68 @@ vis.append('svg:g')
   .attr('d', lineFunc(lineData))
   .attr('stroke', 'blue')
   .attr('stroke-width', 2)
-  .attr('fill', 'none')
+  .attr('fill', 'none');
+  
+var lineFunc2 = d3.svg.line()
+  .x(function(d) {
+    return xRange(d.x);
+  })
+  .y(function(d) {
+    return yRange(d.z);
+  })
+  .interpolate('linear');
+  
+  vis.append('svg:path')
+  .attr('d', lineFunc2(lineData))
+  .attr('stroke', 'purple')
+  .attr('stroke-width', 2)
+  .attr('fill', 'none');
+  
+var lineFunc3 = d3.svg.line()
+  .x(function(d) {
+    return xRange(d.x);
+  })
+  .y(140)
+  .interpolate('linear');
+  
+var lineFunc4 = d3.svg.line()
+  .x(function(d) {
+    return xRange(d.x);
+  })
+  .y(160)
+  .interpolate('linear');
+  
+  vis.append('svg:path')
+  .attr('d', lineFunc3(lineData))
+  .attr('stroke', 'green')
+  .attr('stroke-width', 5)
+  .attr('fill', 'none');
+  
+  vis.append('svg:path')
+  .attr('d', lineFunc4(lineData))
+  .attr('stroke', 'green')
+  .attr('stroke-width', 5)
+  .attr('fill', 'none');
+  
+var area = d3.svg.area()
+  .x(function(d) { 
+    return xRange(d.x); 
+  })
+  .y0(function(d) { 
+    return 140; 
+  })
+  .y1(function(d) { 
+    return 160; 
+  });
+  
+  vis.append('svg:path')
+  .datum(idealTemp)
+  .attr('class', 'area')
+  .attr('d', area)
+  .attr('opacity', 0.5);
+  
+
+  
   
   // var idealTemp = d3.svg.line()
   // .x(function(d) {
@@ -271,11 +388,22 @@ vis.append('svg:g')
   
 //   var yRangeIdealTemp = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([140, 160])
   
-  vis.append('rect')
-  .attr('x', 50)
-  .attr('y', 50)
-  .attr('width', 430) // sets the width of bar
-  .attr('height', 92)
-  .attr('fill', 'green')   // fills the bar with grey color
-  .attr('opacity', 0.5);
+  // vis.append('rect')
+  // .attr('x', function (d) { return d; })
+  // .attr('y', (d3.max(lineData, function(d) {
+  //     return (d.b);
+  //   }) - d3.min(lineData, function(d) {
+  //     return (d.a);
+  //   })))
+  // .attr('height', (d3.max(lineData, function(d) {
+  //     return (d.b);
+  //   }) - d3.min(lineData, function(d) {
+  //     return (d.a);
+  //   })))
+  // // .attr('x', 50)
+  
+  // .attr('width', 430) // sets the width of bar
+  // // .attr('height', 92)
+  // .attr('fill', 'green')   // fills the bar with grey color
+  // .attr('opacity', 0.5);
   
