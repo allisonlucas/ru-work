827c3e0029ec0f05f607ed84cb1c4412463cb40f
Allison Dean <allisonlaureldean@gmail.com>
2016-06-16 23:10:35 +0000

    midterm in progress

--- Projects/midterm_chart/chart.js
+++ Projects/midterm_chart/chart.js
@@ -4,13 +4,12 @@
     
   function chartController () {
     var c = this
+    
     // ==================================================
     // D3 CODE TO POPULATE GRAPH
     // ==================================================
-    function displayGraph (id, width, height, interpolation, animate, updateDelay, transitionDelay) {
+    function displayGraph (id, width, height, margin, interpolation, updateDelay) {
       // create an SVG element inside the #graph div that fills 100% of the div
-      var graph = d3.select(id).append('svg:svg').attr('width', '100%').attr('height', '100%')
-      
       var lineData = {
         day: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
         temp: [140, 143, 145, 131, 141, 140, 144, 147, 148, 150, 151, 151, 148, 147],
@@ -18,56 +17,79 @@
         idealTempMin: [140],
         idealTempMax: [160]
       }
-      // X scale
-      var xRange = d3.scale.linear().domain([d3.min(lineData.day), d3.max(lineData.day)]).range([-5, width])
-      // starting point is -5 so the first value doesn't show and slides off the edge as part of the transition
-      // Y scale
-      var yRange = d3.scale.linear().domain([d3.min(lineData.temp), d3.max(lineData.temp)]).range([0, height])
-      
-      // Plot the temp line & append it
-      var lineFunc = d3.svg.line()
-        .x(function (d, i) {
-          return xRange(lineData.day[i])
-        })
-        .y(function (d, i) {
-          return yRange(lineData.temp[i])
-        })
-        .interpolate(interpolation)
+
+      function draw () {
+        var graph = d3.select(".temp-graph").append('svg:svg').attr("id", "graph").attr('width', '500').attr('height', '250').attr('margin', '45')
         
-      graph.append('svg:path')
-        .attr('d', lineFunc(lineData.day))
+          // X scale
+        var xRange = d3.scale.linear().domain([d3.min(lineData.day), d3.max(lineData.day)]).range([margin, width - margin])
+        // starting point is -5 so the first value doesn't show and slides off the edge as part of the transition
+        // Y scale
+        var yRange = d3.scale.linear().domain([d3.min(lineData.temp) - 20, d3.max(lineData.temp) + 20]).range([height - margin, margin])
         
-      function redrawWithAnimation () {
-        // update with animation
+        // Plot the x and y axes & append
+        var xAxis = d3.svg.axis()
+          .scale(xRange)
+          .tickSize(0)
+        var yAxis = d3.svg.axis()
+          .scale(yRange)
+          .ticks(6)
+          .tickSize(0)
+          .orient('left')
+   
+        graph.append('svg:g')
+          .attr('class', 'x-axis')
+          .attr('transform', 'translate(0,' + (height - margin) + ')')
+          .call(xAxis)
+          .append("text")
+            .attr("x", width / 2 )
+            .attr("y",  margin)
+            .style("text-anchor", "middle")
+            .text("Date")
+   
+        graph.append('svg:g')
+          .attr('class', 'y-axis')
+          .attr('transform', 'translate(' + margin + ',0)')
+          .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - 35)
+            .attr("x",0 - ((height + margin) / 2))
+            .text("Temperature")
+        
+        // Plot the temp line & append it
+        var lineFunc = d3.svg.line()
+          .x(function (d, i) {
+            return xRange(lineData.day[i])
+          })
+          .y(function (d, i) {
+            return yRange(lineData.temp[i])
+          })
+          .interpolate(interpolation)
+          
+        graph.append('svg:path')
+          .attr('d', lineFunc(lineData.temp))
+        
         graph.selectAll('path')
-          .data([lineData.temp]) // set the new data
-          .attr('transform', 'translate(' + xRange(6) + ')') // set the transform to the right by x(1) pixels (6 for the scale we've set) to hide the new value
-          .attr('d', lineFunc(lineData.temp)) // apply the new data values ... but the new value is hidden at this point off the right of the canvas
-          .transition() // start a transition to bring the new value into view
-          .ease('linear')
-          .duration(1500) // for this demo we want a continual slide so set this to the same as the setInterval amount below
-          .attr('transform', 'translate(' + xRange(0) + ')') // animate a slide to the left back to x(0) pixels to reveal the new value
+            .data([lineData.temp]) // set the new data
+            .attr('d', lineFunc(lineData.temp)) // apply the new data values
       }
-      
-      function redrawWithoutAnimation () {
-        // static update without animation
-        graph.selectAll('path')
-          .data([lineData.temp]) // set the new data
-          .attr('d', lineFunc(lineData.temp)) // apply the new data values
-      }
+      draw()
       
       setInterval(function () {
+        
+        lineData.day.shift() // remove the first element of the array
+        var j = lineData.day.length
+        
+        lineData.day.push(j++)
         var v = lineData.temp.shift() // remove the first element of the array
         lineData.temp.push(v) // add a new element to the array (we're just taking the number we just shifted off the front and appending to the end)
-        if (animate) {
-          redrawWithAnimation()
-        } else {
-          redrawWithoutAnimation()
-        }
+        d3.select('#graph').remove()
+        draw()
       }, updateDelay)
-      
     }
-    displayGraph('#graph', 500, 250, 'basis', false, 1500, 750)
+      
+    displayGraph('#graph', 500, 250, 45, 'basis', 1500)
     // ==================================================
     // D3 CODE TO POPULATE DOUGHNUT
     // ==================================================
@@ -79,17 +101,20 @@
       color = ['#2ca02c', '#8c564b']
       
     c.data = [{
-      'label': 'GREEN',
-      'value': [40]
+      label: 'GREEN',
+      value: 53.846154
     }, {
-      'label': 'BROWN',
-      'value': [60]
+      label: 'BROWN',
+      value: 46.153846
     }]
     
     c.weightObj = {
       green: [0.5, 3, 0.75, 1],
       brown: [2, 1, 1, 0.5]
     }
+    
+    console.log('Green starting percent:' + c.data[0].value)
+    console.log('Brown starting percent:' + c.data[1].value)
     
     c.submit = function () {
       console.log('Submit Data')
@@ -105,11 +130,14 @@
         })
         console.log("Green Total: ", greenTotal)
         var percentBrown = (brownTotal / (greenTotal + brownTotal)) * 100
-        console.log("percent brown: ", percentBrown)
-        c.data[1].value.splice(0, 1, percentBrown)
-        console.log(c.data[1].value)
+        var percentGreen = (greenTotal / (greenTotal + brownTotal)) * 100
+        c.data[1].value = percentBrown
+        c.data[0].value = percentGreen
+        console.log('New percent brown: ' + c.data[1].value)
+        console.log('New percent green: ' + c.data[0].value)
       } else {
         c.weightObj.green.push(c.weight)
+        console.log(c.weightObj.green)
         var greenTotal = c.weightObj.green.reduce(function (a, b) {
           return a + b
         })
@@ -117,10 +145,13 @@
         var brownTotal = c.weightObj.brown.reduce(function (a, b) {
           return a + b
         })
+        console.log("Brown Total: ", brownTotal)
         var percentGreen = (greenTotal / (greenTotal + brownTotal)) * 100
-        console.log("Percent Green: ", percentGreen)
-        c.data[0].value.splice(0, 1, percentGreen)
-        
+        var percentBrown = (brownTotal / (greenTotal + brownTotal)) * 100
+        c.data[0].value = percentGreen
+        c.data[1].value = percentBrown
+        console.log('New percent brown: ' + c.data[1].value)
+        console.log('New percent green: ' + c.data[0].value)
       }
       c.weight = ''
       c.color = ''
@@ -168,7 +199,6 @@
         return 'translate(' + arc.centroid(d) + ')'
       })
       .attr('text-anchor', 'middle')
-      // .attr('padding', '10')
       .attr('fill', '#ffffff')
       .text(function (d, i) {
         return c.data[i].label
